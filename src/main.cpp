28d60ae4498a04427f925c4dd331428ae8126c9e
Sergey <perlinsergey@gmail.com>
2024-10-28 18:07:00 +0300

    added: qml.qrc, qml/main.qml edited: cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,20 @@
-#include <QSqlDatabase>
+#include <QQmlApplicationEngine>
 #include <QApplication>
-#include <QDebug>
-#include "Models/DriverModel.h"
-#include "MainWindow.h"
-#include <QPixmap>
-#include <QScreen>
-int main (int argc, char *argv[]) {
-    qputenv("DB_CONNECTION_STRING", "my");
+#include <QObject>
 
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", "my");
-    db.setDatabaseName("racing.db");
 
-    if(!db.open()) {
-        qDebug() << "Error opening database" << db.lastError().text();
-        return -1;
-    }
-
-    qDebug() << "Opened database";
-
+int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    MainWindow window;
+    QQmlApplicationEngine engine;
+    const QUrl qrcUrl("qrc:qml/main.qml");
+    QObject::connect(&engine,   &QQmlApplicationEngine::objectCreated, &app,
+        [qrcUrl](QObject *obj,const QUrl &objUrl) {
+        if(!obj &&  objUrl != qrcUrl)
+            QApplication::exit(-1);
+    }, Qt::QueuedConnection);
 
-    QScreen *screen = QApplication::primaryScreen();
-    QRect rect = screen->geometry();
-    int x = (rect.width() - window.width()) / 2;
-    int y = (rect.height() - window.height()) / 2;
-    window.move(x, y);
-    // window.setAttribute(Qt::WA_TranslucentBackground, true);
-
-        // QPalette pal = window.palette(); pal.setBrush(QPalette::Normal, QPalette::Window,
-        //     QBrush(QPixmap("./bg.png")));
-        // window.setPalette(pal);
-
-
-
-    window.show();
-
-
-    //
-    // DriverModel driverModel;
-    // QVector<Driver> drivers = DriverModel::getAllSortedByName();
-    //
-    // for(int i = 0; i < drivers.count(); i++) {
-    //     qDebug() << drivers[i].getInfo() << '\n';
-    // }
-
+    engine.load(qrcUrl);
 
     return app.exec();
 }
